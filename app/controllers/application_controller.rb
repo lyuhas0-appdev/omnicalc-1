6ffa7a7d7dda0c2dad8ed3a4b00483cb71a67f97
lyuhas0 <lyuhas0@chicagobooth.edu>
2020-07-19 19:37:46 +0000

    square pass

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
-
+  def blank_square_form
+    render({ :template => "calculation_templates/square_form.html.erb"})
+  end
+  def calculate_square
+    @num = params.fetch("number").to_f
+    @square_of_num = @num * @num
+    render({ :template => "calculation_templates/square_results.html.erb"})
+  end
+  def blank_square_root_form
+    render({ :template => "calculation_templates/square_root_form.html.erb"})
+  end
+  def calculate_square_root
+    @num = params.fetch("number").to_f
+    @square_of_num = @num * @num
+    render({ :template => "calculation_templates/square_root_results.html.erb"})
+  end
 end
